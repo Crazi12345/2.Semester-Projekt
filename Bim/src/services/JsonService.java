bbd660cfe1316efc847dde303cf41d5e0b1b31d1
Andreas Trolle <andreas.trolle@hotmail.com>
2022-02-15 12:21:57 +0100

    Simple json added

--- Bim/src/services/JsonService.java
+++ Bim/src/services/JsonService.java
@@ -1,12 +1,25 @@
 package services;
 
 import models.Brand;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
 
-import java.util.List;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class JsonService implements IJsonService {
     @Override
     public Brand deserializeBrand() {
+        try {
+            // Nederste eksempel: https://www.geeksforgeeks.org/parse-json-java/
+            Object obj = new JSONParser().parse(new FileReader("../json/brands.json"));
+        } catch (IOException e) {
+            System.out.println("IOException during JSON parsing.");
+        } catch (ParseException e) {
+            System.out.println("ParseException during JSON parsing.");
+        }
+
         return null;
     }
 
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
 
+import services.JsonService;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	JsonService s = new JsonService();
+    s.deserializeBrand();
     }
 }
